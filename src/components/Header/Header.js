2531c0bd3b063aad8ab369f7f0e0fe6dd53f7072
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-08-29 23:44:49 +0300

    adds convertation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,111 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Header() {
-	const [currencyLeft, setCurrencyLeft] = useState({ value: 1, currency: "UAH" });
-	const [currencyRight, setCurrencyRight] = useState({ value: 0, currency: "USD" });
+	const [currencyLeft, setCurrencyLeft] = useState({ value: 1, currency: "USD" });
+	const [currencyRight, setCurrencyRight] = useState({ value: 0, currency: "UAH" });
 
-	function changesRightCurr(e) {
-		setCurrencyLeft({ ...currencyLeft, value: e.currentTarget.value });
-		setCurrencyRight({ ...currencyRight, value: e.currentTarget.value * 40 });
+	useEffect(() => convertRightValue(currencyLeft.value), [currencyLeft.currency, currencyRight.currency]);
+
+	function convertRightValue(currValue) {
+		if (currencyLeft.currency === "USD" && currencyRight.currency === "UAH") {
+			setCurrencyRight({ ...currencyRight, value: (currValue * 40).toFixed(2) });
+			return;
+		}
+
+		if (currencyLeft.currency === "EUR" && currencyRight.currency === "UAH") {
+			setCurrencyRight({ ...currencyRight, value: (currValue * 42).toFixed(2) });
+			return;
+		}
+
+		if (currencyLeft.currency === "UAH" && currencyRight.currency === "USD") {
+			setCurrencyRight({ ...currencyRight, value: (currValue / 40).toFixed(2) });
+			return;
+		}
+
+		if (currencyLeft.currency === "UAH" && currencyRight.currency === "EUR") {
+			setCurrencyRight({ ...currencyRight, value: (currValue / 42).toFixed(2) });
+			return;
+		}
+
+		if (currencyLeft.currency === "USD" && currencyRight.currency === "EUR") {
+			setCurrencyRight({ ...currencyRight, value: ((currValue * 40) / 42).toFixed(2) });
+			return;
+		}
+
+		if (currencyLeft.currency === "EUR" && currencyRight.currency === "USD") {
+			setCurrencyRight({ ...currencyRight, value: ((currValue * 42) / 40).toFixed(2) });
+			return;
+		}
+
+		setCurrencyRight({ ...currencyRight, value: currValue });
 	}
 
-	function changesLeftCurr(e) {}
+	function convertLeftValue(currValue) {
+		if (currencyLeft.currency === "USD" && currencyRight.currency === "UAH") {
+			setCurrencyLeft({ ...currencyLeft, value: (currValue / 40).toFixed(2) });
+			return;
+		}
+
+		if (currencyLeft.currency === "EUR" && currencyRight.currency === "UAH") {
+			setCurrencyLeft({ ...currencyLeft, value: (currValue / 42).toFixed(2) });
+			return;
+		}
+
+		if (currencyLeft.currency === "UAH" && currencyRight.currency === "USD") {
+			setCurrencyLeft({ ...currencyLeft, value: (currValue * 40).toFixed(2) });
+			return;
+		}
+
+		if (currencyLeft.currency === "UAH" && currencyRight.currency === "EUR") {
+			setCurrencyLeft({ ...currencyLeft, value: (currValue * 42).toFixed(2) });
+			return;
+		}
+
+		if (currencyLeft.currency === "USD" && currencyRight.currency === "EUR") {
+			setCurrencyLeft({ ...currencyLeft, value: ((currValue / 40) * 42).toFixed(2) });
+			return;
+		}
+
+		if (currencyLeft.currency === "EUR" && currencyRight.currency === "USD") {
+			setCurrencyLeft({ ...currencyLeft, value: ((currValue / 42) * 40).toFixed(2) });
+			return;
+		}
+
+		setCurrencyLeft({ ...currencyLeft, value: currValue });
+	}
+
+	function changeLeftValue(e) {
+		setCurrencyLeft({ ...currencyLeft, value: e.currentTarget.value });
+		convertRightValue(e.currentTarget.value);
+	}
+
+	function changeRightValue(e) {
+		setCurrencyRight({ ...currencyRight, value: e.currentTarget.value });
+		convertLeftValue(e.currentTarget.value);
+	}
+
+	function changeLeftCurrency(e) {
+		setCurrencyLeft({ ...currencyLeft, currency: e.currentTarget.value });
+	}
+
+	function changeRightCurrency(e) {
+		setCurrencyRight({ ...currencyRight, currency: e.currentTarget.value });
+	}
 
 	return (
 		<div>
-			<input type="number" name="currency-left" value={currencyLeft.value} onChange={changesRightCurr} />
-			<select name="currency-left">
-				<option value="UAH" autoFocus>
-					UAH
-				</option>
-				<option value="USD" disabled>
-					USD
-				</option>
+			<input type="number" value={currencyLeft.value} onChange={changeLeftValue} />
+			<select value={currencyLeft.currency} onChange={changeLeftCurrency}>
+				<option value="UAH">UAH</option>
+				<option value="USD">USD</option>
 				<option value="EUR">EUR</option>
 			</select>
 			=
-			<input type="number" name="currency-right" value={currencyRight.value} onChange={changesLeftCurr} />
-			<select name="currency-right">
-				<option value="UAH" disabled>
-					UAH
-				</option>
-				<option value="USD" autoFocus>
-					USD
-				</option>
+			<input type="number" value={currencyRight.value} onChange={changeRightValue} />
+			<select value={currencyRight.currency} onChange={changeRightCurrency}>
+				<option value="USD">USD</option>
 				<option value="EUR">EUR</option>
+				<option value="UAH">UAH</option>
 			</select>
 		</div>
 	);
